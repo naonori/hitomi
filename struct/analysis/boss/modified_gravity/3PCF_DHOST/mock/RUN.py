e8d7ef75e089d1cad039106245a24c57775d9930
Naonori Sugiyama <nao.s.sugiyama@gmail.com>
2023-02-13 14:33:22 +0900

    Add files via upload

---
+++ struct/analysis/boss/modified_gravity/3PCF_DHOST/mock/RUN.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os
+import numpy as np
+import scipy as sp
+import subprocess
+
+output_dir = "chains_base" #
+#output_dir = "chains_main" # chains_base or chains_main
+try:
+    os.mkdir(output_dir)
+except:
+    pass
+
+MOCK = []
+for i in range(100):
+    MOCK.append("mock_%04d" % (i+1))
+
+for param in ["DHOST_North_zbin1", "DHOST_South_zbin1", "DHOST_North_zbin3", "DHOST_South_zbin3",\
+              "DHOST_zbin1", "DHOST_zbin3", "DHOST_North", "DHOST_South", "DHOST"]:
+
+    try:
+        os.mkdir("%s/%s" % (output_dir, param))
+    except:
+        pass
+    
+#    for data_type in ["galaxy"] + MOCK:
+    for data_type in MOCK:
+    
+        print("%s_%s" % (data_type, param))
+
+        fr = open("run_base.sh", "r")
+        AA = fr.readlines()
+        fr.close()
+        
+        if output_dir == "chains_base":
+            AA[8] = "python MontePython.py run -o %s/%s/%s_%s -p input/%s_%s.param --conf=my.conf --superupdate 20 -N 200000" % (output_dir, param, data_type, param, data_type, param)
+    
+        elif output_dir == "chains_main":
+            AA[8] = "python MontePython.py run -o %s/%s/%s_%s -p input/%s_%s.param -b chains_base/%s/%s_%s/%s_%s.bestfit -c chains_base/%s/%s_%s/%s_%s.covmat --conf=my.conf --superupdate 20 -N 200000" % (output_dir, param, data_type, param, data_type, param, param, data_type, param, data_type, param, param, data_type, param, data_type, param)
+        
+        fw = open("run_new.sh", "w")
+        fw.writelines(AA)
+        fw.close()
+        
+        subprocess.call(["chmod", "u+x", "run_new.sh"])
+
+        if output_dir == "chains_main" and data_type == "galaxy":
+            for i in range(8):
+                subprocess.call(["qsub", "run_new.sh"])
+
+        else:
+            subprocess.call(["qsub", "run_new.sh"])
+        
---
+++ struct/analysis/boss/modified_gravity/3PCF_DHOST/mock/create_likelihoods.py
@@ -0,0 +1,99 @@
+#######################
+# Purpose of this code
+#######################
+# The code makes the likelihood directory for mock catalogues. 
+#######################
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys, os
+import numpy as np
+import shutil
+
+MOCK = []
+for i in range(100):
+    MOCK.append("mock_%04d" % (i+1))
+
+for data_type in MOCK:
+
+    model_type = "mock"
+
+    for lkl in ["DHOST_North_zbin1_case7", "DHOST_North_zbin1_case7_NS", "DHOST_North_zbin1_case7_ZBIN", "DHOST_North_zbin1_case7_TOTAL",\
+                "DHOST_South_zbin1_case7", "DHOST_South_zbin1_case7_NS", "DHOST_South_zbin1_case7_ZBIN", "DHOST_South_zbin1_case7_TOTAL",\
+                "DHOST_North_zbin3_case7", "DHOST_North_zbin3_case7_NS", "DHOST_North_zbin3_case7_ZBIN", "DHOST_North_zbin3_case7_TOTAL",\
+                "DHOST_South_zbin3_case7", "DHOST_South_zbin3_case7_NS", "DHOST_South_zbin3_case7_ZBIN", "DHOST_South_zbin3_case7_TOTAL"]:
+
+        ##############################
+        # Make a likelihoods directory
+        
+        likelihoods = "%s_%s"  % (data_type, lkl)
+        
+        print(likelihoods)
+        
+        try:
+            shutil.rmtree("likelihoods/%s" % likelihoods)
+        except:
+            pass
+        
+        shutil.copytree("likelihoods/galaxy_%s" % lkl, "likelihoods/%s" % likelihoods)
+        shutil.move("likelihoods/%s/galaxy_%s.data" % (likelihoods, lkl), "likelihoods/%s/%s.data" % (likelihoods, likelihoods))
+        
+        ##############################
+        # Edit the .data file in the likelihood directory.
+        
+        fr = open("likelihoods/%s/%s.data" % (likelihoods, likelihoods), "r")
+        AA = fr.readlines()
+        fr.close()
+ 
+        for i in range(len(AA)):
+            AA[i] = AA[i].replace("galaxy_%s" % lkl, likelihoods)
+        
+        for i in range(len(AA)):
+            if AA[i].find("%s.data_type" % likelihoods) >= 0:
+                AA[i] = '%s.data_type  = "%s"\n' % (likelihoods, data_type)
+                
+            if AA[i].find("%s.model_type" % likelihoods) >= 0:
+                AA[i] = '%s.model_type  = "%s"\n' % (likelihoods, model_type)
+        
+        fw = open("likelihoods/%s/%s.data" % (likelihoods, likelihoods), "w")
+        fw.writelines(AA)
+        fw.close()
+         
+        ##############################
+        # Edit the "__init__.py" in the likelihood directory.
+        
+        fr = open("likelihoods/%s/__init__.py" % (likelihoods), "r")
+        AA = fr.readlines()
+        fr.close()
+
+        for i in range(len(AA)):
+            AA[i] = AA[i].replace("galaxy_%s" % lkl, likelihoods)
+
+        fw = open("likelihoods/%s/__init__.py" % (likelihoods), "w")
+        fw.writelines(AA)
+        fw.close()
+ 
+
+##############################
+# Copy and edit "input/xxx.param".
+
+for data_type in MOCK:
+
+    for param in ["DHOST.param", "DHOST_North.param", "DHOST_South.param", "DHOST_zbin1.param", "DHOST_zbin3.param",\
+                  "DHOST_North_zbin1.param", "DHOST_South_zbin1.param", "DHOST_North_zbin3.param", "DHOST_South_zbin3.param"]:
+
+        shutil.copy("input/galaxy_%s" % param, "input/%s_%s" % (data_type, param))
+        
+        fr = open("input/%s_%s" % (data_type, param), "r")
+        AA = fr.readlines()
+        fr.close()
+         
+        for i in range(len(AA)):
+            AA[i] = AA[i].replace("galaxy", data_type)
+        
+        fw = open("input/%s_%s" % (data_type, param), "w")
+        fw.writelines(AA)
+        fw.close()
+                
+                
